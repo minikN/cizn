07b8cc8cf33ddd82ff8b33476900f6a2349a8971
Demis Balbach <db@minikn.xyz>
2024-06-16 15:21:22 +0200

    Creates renames writeFile and ReadFile; creates wrapper functions; Renames local variables

--- src/file.sh
+++ src/file.sh
@@ -1,13 +1,38 @@
 ###
+### Will write the `withRealFile' function to the
+### current derivation with the given arguments
+###
+function withFile() {
+	# shellcheck disable=SC2154
+	tee -a "$tempFile" <<EOF >/dev/null
+withRealFile "${1}" \
+"${2}"
+EOF
+}
+
+###
+### Will write the `createRealFile' function to the
+### current derivation with the given arguments
+###
+function createFile() {
+	tee -a "$tempFile" <<EOF >/dev/null
+createRealFile "${1}" \
+"${2}"
+EOF
+}
+
+###
 ### Opens file at `$1' and appends `$2' to it.
 ### File has to exist. Will return `1' otherwise.
 ###
-function withFile() {
+function withRealFile() {
 	local filePath
 	local stateFilePath
+	local curGenPath
 
 	filePath="${CUR_GEN_ROOT}/files$(readlink -fm "$1")"
-	stateFilePath="${filePath#*"${CUR_GEN}/"}"
+	curGenPath=$(readlink -fm "${CUR_GEN}")
+	stateFilePath="${filePath#*"${curGenPath}/"}"
 
 	if [ ! -f "$filePath" ]; then
 		logError "File $stateFilePath doesn't exist."
@@ -25,12 +50,14 @@ EOF
 ### File has to not exist. Will return `1'
 ### otherwise.
 ###
-function createFile() {
+function createRealFile() {
 	local filePath
 	local stateFilePath
+	local curGenPath
 
 	filePath="${CUR_GEN_ROOT}/files$(readlink -fm "$1")"
-	stateFilePath="${filePath#*"${CUR_GEN}/"}"
+	curGenPath=$(readlink -fm "${CUR_GEN}")
+	stateFilePath="${filePath#*"${curGenPath}/"}"
 
 	if [ -f "$filePath" ]; then
 		logError "File $filePath already exists."
