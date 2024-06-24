e6afef36232f186d863c20f46b96c5e5c0d147aa
Demis Balbach <db@minikn.xyz>
2024-06-25 00:08:58 +0200

    cizn: Adds modules; options; api to state.

--- src/app/core/state.js
+++ src/app/core/state.js
@@ -1,52 +1,11 @@
+/* global process */
 import { setNamespace, defineNamespace } from "@lib/composition/namespace.js"
+import { defineProp } from "@lib/composition/property.js"
 import { compose } from "@lib/util/index.js"
 import G from '@lib/static.js'
-import 'zx/globals'
-// /* global process */
-// import { readlink, realpath, readdir, mkdir } from 'node:fs/promises'
-// import * as path from "node:path"
+import { $ } from 'execa'
 
-// export const testFn = () => {
-//   console.log('test')
-// }
-
-// /**
-//  * Initializes the application state.
-//  *
-//  * @param {PathLike} config
-//  * @param {import('@commander-js/extra-typings').Command} command
-//  * @returns {Promise<void>}
-//  * @constructor
-//  */
-// const State = async ({ config: configPath }, command) => {
-//   return new Promise(async (resolve) => {
-//     const config = await realpath(configPath)
-//     const configRoot = path.dirname(config)
-//     const stateRoot = process.env?.XDG_STATE_HOME || configRoot
-//     const generationsRoot = `${stateRoot}/cizn/generations`
-//     const derivationsRoot = `${stateRoot}/cizn/derivations`
-//     let lastWorkingGeneration = null
-//     let currentGenerationRoot = null
-//     let currentGeneration = null
-
-//     await mkdir(generationsRoot, { recursive: true })
-//     await mkdir(derivationsRoot, { recursive: true })
-
-//     resolve({
-//       config,
-//       configRoot,
-//       stateRoot,
-//       generationsRoot,
-//       derivationsRoot,
-//       lastWorkingGeneration,
-//       currentGenerationRoot,
-//       currentGeneration,
-//     })
-//   }).catch(e => command.error(e.message))
-// }
-
-// export default State
-const { CONFIG, GENERATION, DERIVATION, CURRENT, ROOT } = G
+const { MODULES, OPTIONS, CONFIG, GENERATION, DERIVATION, CURRENT, ROOT, API } = G
 
 
 const state = async (obj) => {
@@ -61,6 +20,8 @@ const state = async (obj) => {
 
   const stateComposition = compose(
     defineNamespace(CONFIG),
+    defineProp(MODULES, {}),
+    defineProp(OPTIONS, {}),
     setNamespace(CONFIG, {
       [CURRENT]: currentConfig,
       [ROOT]: configRoot,
@@ -69,11 +30,13 @@ const state = async (obj) => {
     setNamespace(DERIVATION, {
       [CURRENT]: derivationsRoot,
       [ROOT]: null,
+      [API]: null,
     }),
     defineNamespace(GENERATION),
     setNamespace(GENERATION, {
       [CURRENT]: generationsRoot,
       [ROOT]: null,
+      [API]: null,
     }),
 
   )(obj)
