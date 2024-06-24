2c0e0ee1c4de1980d1452ce25ce1482c88c548e2
Demis Balbach <db@minikn.xyz>
2024-06-25 00:11:49 +0200

    cizn: Started implementation of build command.

--- src/app/adapter/cli/api/build.js
+++ src/app/adapter/cli/api/build.js
@@ -1,9 +1,29 @@
+import G from '@lib/static.js'
+import { $ } from 'execa'
+import { access, constants, lstatSync } from 'node:fs'
+const { ADAPTER, STATE, DERIVATION, LOG, API } = G
+
 const build = app => async (options, command) => {
-//   const { ...state } = await State({ config }, command)
-//   const configPath = await realpath(config)
-//   const stuff = await import(/* webpackIgnore: true */configPath)
-//   console.log('stuff', stuff)
-  console.log('build')
+  const log = app[ADAPTER][LOG][API]
+  const derivation = app[STATE][DERIVATION]
+
+  const { source } = options
+  const { stdout: configPath } = await $`realpath ${source}`
+  log.info({ message: `Reading config file ${configPath} ...` })
+
+  access(configPath, constants.F_OK, (err) => {
+    if (err || !lstatSync(configPath).isFile()) {
+      log.error({ message: `${configPath} does not exist. Aborting.` })
+    }
+  })
+
+  try {
+    const { config } = await import(`${configPath}`)
+    await derivation[API].make({ config })
+
+  } catch (e) {
+    log.error({ message: `Error reading config file: ${e.message}` })
+  }
 }
 
 export default build
