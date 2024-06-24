11b51568fbe2e7d485dd7367498b9ab79cac011e
Demis Balbach <db@minikn.xyz>
2024-06-25 00:09:36 +0200

    lib: Adds modules; options symbols

--- src/lib/static.js
+++ src/lib/static.js
@@ -1,13 +1,16 @@
 
 export default Object.freeze({
   CLI: Symbol('cli'),
+  LOG: Symbol('log'),
   API: Symbol('api'),
   STATE: Symbol('state'),
-  
+
   CONFIG: Symbol('config'),
+  MODULES: Symbol('modules'),
+  OPTIONS: Symbol('options'),
   GENERATION: Symbol('generation'),
   DERIVATION: Symbol('derivation'),
-  
+
   ROOT: Symbol('root'),
   CURRENT: Symbol('current'),
 
