bb33a2240d4c1b7a38300faf2760bf3aba9da5b2
Demis Balbach <db@minikn.xyz>
2024-07-23 09:12:01 +0200

    cli: Implement global config option

--- src/app/adapter/cli/api/init.ts
+++ src/app/adapter/cli/api/init.ts
@@ -1,13 +1,44 @@
+import { Command } from "@commander-js/extra-typings"
+
+// /**
+//  *
+//  * @param flags
+//  * @param name
+//  * @param args
+//  * @returns
+//  */
+// function getSingleOption (flags, name, args) {
+//   const findOne = new Command
+//   findOne
+//     .allowUnknownOption()
+//     .helpOption(false)
+//     .option(flags)
+//   findOne.parse(args)
+//   return findOne.opts()[name]
+// }
+
 const init = (App: Cizn.Application) => () => {
   const { Cli } = App.Adapter
   const {
-    _name, _version, Api, Program,
+    _name, _version, Api, Program, Global,
   } = Cli
 
-  Program.version(_version)
-  Program.description(_name)
+  Global
+    .allowUnknownOption()
+    .helpOption(false)
+    .option('-c, --config [path]', 'Path to the configuration file', undefined)
+    .parse(process.argv)
 
-  Program.command('build')
+  // TODO: Save config option in state, then read the config file
+  const globalOpts = Global.opts()
+
+  Program
+    .version(_version)
+    .description(_name)
+    .option('-c, --config [path]', 'Path to the configuration file', undefined)
+
+  Program
+    .command('build')
     .description('Build a given configuration')
     .argument('[environment]', 'Environment to build. Can be "home" or "system". If ommitted, both will be built.')
     .option('-s, --source <string>', 'path to the source file', './config.js')
--- src/app/adapter/cli/index.ts
+++ src/app/adapter/cli/index.ts
@@ -1,7 +1,6 @@
 import { pipe } from "@lib/util/index.js"
 import { defineImmutableProp } from "@lib/composition/property.js"
 import { APP_NAME, APP_VERSION } from "@cizn/index.js"
-import { program } from "@commander-js/extra-typings"
 import { defineNamespace, setNamespace } from "@lib/composition/namespace.js"
 import cliApi from "@cizn/adapter/cli/api/index.js"
 import { Command } from "commander"
@@ -10,6 +9,7 @@ export type Cli = {
   readonly _name: string,
   readonly _version: string,
   Program: Command
+  Global: Command
   Api: Cizn.Adapter.Cli.Api
 }
 
@@ -23,11 +23,11 @@ const cliAdapter = (app: Cizn.Application): Cizn.Adapter.Cli => {
   const adapterComposition = pipe<Cizn.Adapter.Cli>(
     defineImmutableProp('_name', APP_NAME),
     defineImmutableProp('_version', APP_VERSION),
-    defineImmutableProp('Program', program),
+    defineImmutableProp('Program', new Command),
+    defineImmutableProp('Global', new Command),
     defineNamespace('Api'),
     setNamespace('Api', cliApi(app)),
   )(<Cizn.Adapter.Cli>{})
-
 
   return adapterComposition
 }
