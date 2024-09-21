df62c9d6a5bb5fcbd2a7edfcb9e0e859b117b94d
Demis Balbach <db@minikn.xyz>
2024-09-21 12:43:24 +0200

    Function: Adds doc block

--- src/lib/composition/function.ts
+++ src/lib/composition/function.ts
@@ -1,14 +1,127 @@
+import { _dual } from "@lib/composition/pipe"
+import {
+  Failure, Result, Success,
+} from "@lib/composition/result"
+
+
+const match = <E1, A, E2, B>(nextFunction: (a: A) => Result<E2, B>, input: Result<E1, A>) => {
+  switch (input._tag) {
+    case 'error':
+      return input
+    case 'value':
+      return nextFunction(input.value)
+    default:
+      const _exhaustive: never = input
+      return _exhaustive
+  }
+}
+
+
 /**
- * TEE ADAPTER FUNCTION
+ * MAP ADAPTER FUNCTION
  *
- * Converts a dead-end function, meaning a function that returns {@code void}
- * into usable "track-segment" by executing the void function with the output
- * of the previous function and then returns that output to the next function
+ * @note: Based on https://github.com/Effect-TS/effect/blob/main/packages/effect/src/internal/core.ts#L754
  *
- * @param deadEndFunction
+ * This function maps a switch function. A switch function is a function that
+ * has one input (data) but may return two different things, either data
+ * (happy path) or an error (bad path).
+ *
+ * Map will return a function that accepts the output of the previous function
+ * in the pipeline ({@code input}) and will match it (using {@link match}), if
+ * it matches the happy case, it will execute {@code nextFunction} with the
+ * output. If it matches the bad case, it will return the error immediately and
+ * not pass it on to the function it maps.
+ *
+ * In addition to that, it will also attempt to flatten the output. If
+ * {@code input} is a {@link Result}, and the output of {@code nextFunction} is
+ * also a {@link Result}, without flattening the return type would be
+ * ```
+ * Result<Error, Result<previousError, value>>
+ * ```
+ * By flattening the result, the return type is
+ * ```
+ * Result<Error | previousError, value>
+ * ```
+ *
+ * @example
+ * const app = pipe(
+ *   Success({}), // empty object
+ *   map(doSomething)
+ * )
+ *
+ * @param {Function} nextFunction the next function to execute
+ * @param {Result} input          output of the previous function
+ *
+ * @constant
+ * @name map
+ * @kind variable
+ * @type {{ <A, E2, B>(nextFunction: (a: A) => Result<E2, B> | Promise<Result<E2, B>>): <E1>(input: Result<E1, A>) => Result<E1 | E2, B>; <E1, A, E2, B>(input: Result<E1, A>, nextFunction: (a: A) => Result<E2, B>): Result<E1 | E2, B>; }}
+ * @exports
+ */
+export const map: {
+  <A, E2, B>(nextFunction: (a: A) => Result<E2, B> | Promise<Result<E2, B>>): <E1>(input: Result<E1, A>) => Result<E1 | E2, B>
+  <E1, A, E2, B>(input: Result<E1, A>, nextFunction: (a: A) => Result<E2, B>): Result<E1 | E2, B>
+} = _dual(2,
+  <E1, A, E2, B>(input: Result<E1, A>, nextFunction: (a: A) => Result<E2, B>): Result<E1 | E2, B> => match(
+    nextFunction,
+    input,
+  ))
+
+
+/**
+ * BIND ADAPTER FUNCTION
+ *
+ * Binds a single function. A single function is a function that has an input and
+ * only one possible output. Meaning it cannot fail. It will match the `previousValue`
+ * by passing it the output of the single function wrappen in a {@link Success}.
+
+ * @param {Function} singleFunction
+ *
+ * @constant
+ * @name bind
+ * @kind variable
+ * @type {<E1, A, B>(singleFunction: (a: A) => B) => (previousValue: Result<E1, A>) => Result<E1, B>}
+ * @exports
+ */
+export const bind = <E1, A, B>(singleFunction: (a: A) => B) => (previousValue: Result<E1, A>): Result<E1, B> => match(
+  value => Success(singleFunction(value)),
+  previousValue,
+)
+
+/**
+ * GUARD ADAPTER FUNCTION
+ *
+ * Wraps the {@param switchFunction} in a try/catch block and if an error is
+ * thrown, it will wrap it in a {@link Failure} and return it. Otherwise it
+ * will return the result of the function.
+ *
+ * @param {Function} switchFunction Function to wrap
+ * @param {Object} [errors] Mapping between error types and thrown errors
  * @returns
  */
-export const tee = <T>(deadEndFunction: (a: T) => void) => (previousValue: T) => {
+const guard = <L, R, T extends Error>(
+  switchFunction: (a: R) => Result<L, R> | Promise<Result<L, R>>,
+  errors?: {[key: string]: T},
+) => async (previousValue: R) => {
+    try {
+      return await switchFunction(previousValue)
+    } catch (e: any) {
+      return Failure(errors?.[e.code] || e as L)
+    }
+  }
+
+/**
+ * TAP ADAPTER FUNCTION
+ *
+ * Executes a `deadEndFunction`, meaning a function that returns `void`
+ * and returns the outpur of the previous function to the next function
+ *
+ * @param {(a: T) => void} deadEndFunction
+ * @returns {T} previousValue
+ */
+
+
+export const tap = <T>(deadEndFunction: (a: T) => void) => (previousValue: T) => {
   deadEndFunction(previousValue)
 
   return previousValue
