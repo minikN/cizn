ce2105935daf00a091d67ef59f34ec1ba30aa73e
Demis Balbach <db@minikn.xyz>
2024-10-10 22:12:08 +0200

    manager: Refactors cli managers init function

--- src/lib/managers/cli/api/init.ts
+++ src/lib/managers/cli/api/init.ts
@@ -1,36 +1,108 @@
+import { bind } from "@lib/composition/function"
+import { pipe } from "@lib/composition/pipe"
 import { Result, Success } from "@lib/composition/result"
+import { Command } from "commander"
 
-const init = (app: Cizn.Application) => (): Result<never, Cizn.Application> => {
-  const { Cli } = app.Manager
-  const {
-    _name, _version, Api, Program, Global,
-  } = Cli
+/**
+ * Sets a global option.
+ *
+ * @param {string} flags    fflags for the option
+ * @param {string} desc     description of the option
+ * @param {*} defaultValue  default value for the option
+ */
+const setOption = (flags: string, desc: string, defaultValue: any) => (app: Cizn.Application): Cizn.Application => {
+  const { Manager: { Cli } } = app
+  Cli.Global.option(flags, desc, defaultValue)
+  Cli.Program.option(flags, desc, defaultValue)
 
-  Global
+  return app
+}
+
+/**
+ * Sets a command description.
+ *
+ * @param {string} desc description of the command
+ */
+const setDescription = (desc: string) => (cmd: Command): Command => cmd.description(desc)
+
+/**
+ * Sets a command argument.
+ *
+ * @param {string} flag flag for the argument
+ * @param {string} desc description of the argument
+ */
+const setArgument = (flag: string, desc: string) => (cmd: Command): Command => cmd.argument(flag, desc)
+
+/**
+ * Sets a comman action
+ * @param {Function} handler handler for the action
+ */
+const setAction = (handler: (...args: any) => void) => (cmd: Command): Command => cmd.action(handler)
+
+/**
+ * Sets a command option.
+ *
+ * @param {string} flags    fflags for the option
+ * @param {string} desc     description of the option
+ * @param {*} defaultValue  default value for the option
+ */
+const setCommandOption = (flags: string, desc: string, defaultValue: any) => (cmd: Command): Command => cmd.option(flags, desc, defaultValue)
+
+
+/**
+ * Initializes a command.
+ *
+ * @param {Function} fn the callback initializing the command
+ */
+const setSettings = (fn: (a: Cizn.Manager.Cli) => void) => (app: Cizn.Application): Cizn.Application => {
+  const { Manager: { Cli } } = app
+  fn(Cli)
+
+  return app
+}
+
+/**
+ * Sets global settings.
+ */
+const setGlobalSettings = setSettings((Cli) => {
+  Cli.Global
     .allowUnknownOption()
     .helpOption(false)
-    .option('-c, --config [path]', 'Path to the configuration file', undefined)
-    .parse(process.argv)
+})
 
-  const globalOpts = Global.opts()
+/**
+ * Sets program specific settings.
+ */
+const setProgramSettings = setSettings((Cli) => {
+  Cli.Program.version(Cli._version)
+  Cli.Program.description(Cli._name)
+})
 
-  globalOpts.config && (app.State.Config.Current = <string>globalOpts.config)
+/**
+ * Sets up the `build` command.
+ *
+ * @param {Cizn.Application} app the application
+ */
+const setBuildCommand = (app: Cizn.Application) => pipe(
+  Success(app.Manager.Cli.Program.command('build')),
+  bind(setDescription('Build a given configuration')),
+  bind(setArgument('[environment]', 'Environment to build. Can be "home" or "system". If ommitted, both will be built.')),
+  bind(setCommandOption('-s, --source <string>', 'path to the source file', './config.js')),
+  bind(setAction(app.Manager.Cli.Api.build)),
+  () => app,
+)
 
-  // Parses global options, that come before the command
-  Program
-    .version(_version)
-    .description(_name)
-    .option('-c, --config [path]', 'Path to the configuration file', undefined)
-
-  // Parses command and command-specific options/arguments
-  Program
-    .command('build')
-    .description('Build a given configuration')
-    .argument('[environment]', 'Environment to build. Can be "home" or "system". If ommitted, both will be built.')
-    .option('-s, --source <string>', 'path to the source file', './config.js')
-    .action(Api.build)
-
-  return Success(app)
-}
+/**
+ * Initializes the CLI adapter.
+ *
+ * @param {Cizn.Application} app the application
+ */
+const init = (app: Cizn.Application) => (): Result<never, Cizn.Application> => pipe(
+  Success(app),
+  bind(setGlobalSettings),
+  bind(setProgramSettings),
+  bind(setOption('-c, --config [path]', 'Path to the configuration file', undefined)),
+  bind(setBuildCommand),
+)
 
 export default init
