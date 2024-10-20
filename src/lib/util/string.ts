4801407014338d8307d9a12b8fb3401f6677da8e
Demis Balbach <db@minikn.xyz>
2024-10-20 13:28:41 +0200

    utils: Adds makeHash, getHash, sanitizeMultilineString

--- src/lib/util/string.ts
+++ src/lib/util/string.ts
@@ -1,3 +1,6 @@
+import crypto from 'crypto'
+import { getFileName, isStr } from '.'
+
 /**
  * Will convert {@param str} to camel case.
  *
@@ -14,3 +17,35 @@ export const toCamelCase = (str: string) => str.replace(/(?:^\w|[A-Z]|\b\w)/g, (
  * @returns {string}
  */
 export const sanitize = (str: string) => JSON.stringify(str).replace(/\W/g, '')
+
+/**
+ * Sanitizes `str` by removing comments and empty lines from it.
+ *
+ * @param {string} str string representation of a function
+ * @returns {string} sanitized string
+ */
+export const sanitizeMultilineString = (str: string) => str
+  .replace(/\/\*(.|[\r\n])*?\*\//g, '') // multiline comments
+  .replace(/\/\/.*/gm, '') // normal comments
+  .replace(/^\s*\n/gm, '') // empty lines
+
+/**
+ * Will return a hash for `string`.
+ *
+ * @param {string|object} input the input to hash
+ * @returns {string}
+ */
+export const makeHash = (input: string | object) => crypto
+  .createHash('md5')
+  .update(isStr(input)
+    ? <string>input
+    : JSON.stringify(input))
+  .digest('hex')
+
+/**
+   *  Will return the hash that is contained in `input`.
+   *
+   * @param {string} input  the path to get the filename from
+   * @returns {string} filename
+   */
+export const getHash = (input: string) => getFileName(input)?.split?.('-')?.pop?.() || ''
