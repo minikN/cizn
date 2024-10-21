5696e95563c989621beb419b15bf09a3c1f20557
Demis Balbach <db@minikn.xyz>
2024-10-21 19:52:28 +0200

    cizn: Let's get return fully computed derivation tree.

--- src/app/core/derivation/api/get.ts
+++ src/app/core/derivation/api/get.ts
@@ -1,14 +1,30 @@
 import { Derivation } from '@cizn/core/state'
+import { getHash } from '@lib/util/string'
 import { readdir, readFile } from 'node:fs/promises'
 
 /**
- * Returns the contents of a derivation if it exists or `null` if it doesn't.
+ * Returns the derivation that matches `hash`.
+ *
+ * Will firstly look through the list of already computed derivations amongst
+ * `Derivation.State.Built`. If a derivation amongst them is found, it'll
+ * return it.
+ *
+ * If not, it'll look through the already built derivations on the file system
+ * and find the one matching the hash. After that, it'll recursively call itself
+ * with each of its `inputs`. This will eventually return a complete tree of
+ * the derivation.
  *
  * @param {Cizn.Application} app the application
  * @returns {Derivation | null}
  */
 const get = (app: Cizn.Application): Cizn.Application.State.Derivation.Api['get'] => async ({ hash }) => {
   const { Derivation } = app.State
+
+  const computedDerivation = Derivation.State.Built.find(x => x.hash === hash)
+
+  if (computedDerivation) {
+    return computedDerivation
+  }
 
   const derivations = await readdir(Derivation.Root)
 
@@ -22,9 +38,17 @@ const get = (app: Cizn.Application): Cizn.Application.State.Derivation.Api['get'
 
   const derivationPath = `${Derivation.Root}/${existingDerivation}`
   const derivationFileContent = await readFile(`${derivationPath}`)
-  const derivationContent = JSON.parse(derivationFileContent.toString())
+  const derivationContent = JSON.parse(derivationFileContent.toString()) as Derivation
 
-  return <Derivation>derivationContent
+  for (let i = 0; i < derivationContent.inputs.length; i++) {
+    const inputDerivation = derivationContent.inputs[i]
+    const hash = getHash(inputDerivation.path)
+    const foundDerivation = await Derivation.Api.get({ hash })
+
+    foundDerivation && (derivationContent.inputs[i] = foundDerivation)
+  }
+
+  return derivationContent
 }
 
 export default get
