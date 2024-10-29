d0a020e61368152db9c3083901444067259748df
Demis Balbach <db@minikn.xyz>
2024-10-29 22:31:19 +0100

    lib: Adds new error types

--- src/lib/errors/index.ts
+++ src/lib/errors/index.ts
@@ -1,16 +1,19 @@
-
 export type DefaultErrorTypes =
     // Generic errors
     | 'ENOENT'
     | 'ERR_INVALID_ARG_TYPE'
     | 'EACCESS'
+    | 'EEXIST'
     // Application errors
     | 'NO_PATH_GIVEN'
+    | 'NO_TARGET_GIVEN'
     | 'NO_CONTENT_GIVEN'
     | 'INVALID_CONTENT_GIVEN'
     | 'INCORRECT_PATH_GIVEN'
     | 'NOT_READABLE_FILE'
     | 'NOT_A_FILE'
+    | 'NOT_A_DIR'
+    | 'NOT_A_SYMLINK'
 
 export type CiznError<E extends DefaultErrorTypes> = {
     readonly name: E,
@@ -22,14 +25,22 @@ export type CiznError<E extends DefaultErrorTypes> = {
 
 const defaultErrorMessages = Object.freeze({
   NO_PATH_GIVEN: 'No path given',
+  NO_TARGET_GIVEN: 'No target given',
   NO_CONTENT_GIVEN: 'No content given',
+
   INVALID_CONTENT_GIVEN: 'Invalid content given',
   INCORRECT_PATH_GIVEN: 'Incorrect path given',
+
   ENOENT: 'No such file or directory',
   EACCESS: 'Insufficient file permissions',
+  EEXIST: 'Target already exists',
+
   ERR_INVALID_ARG_TYPE: 'Invalid number of arguments',
+
   NOT_READABLE_FILE: 'File doesn\'t exist or isn\'t readable',
   NOT_A_FILE: 'Given path is not a file',
+  NOT_A_DIR: 'Given path is not a directory',
+  NOT_A_SYMLINK: 'Given path is not a symbolic link',
 })
 
 type OptionalErrorProps = {
@@ -50,6 +61,7 @@ const _error = <E extends DefaultErrorTypes>(name: E, props?: OptionalErrorProps
 
 export const Error: <E extends DefaultErrorTypes>(name: E) => CiznError<E> = _error
 export const ErrorWith: <E extends DefaultErrorTypes>(name: E, options: OptionalErrorProps) => CiznError<E> = _error
-export const ErrorAs = <E extends DefaultErrorTypes>(name: E, overrides?: OptionalErrorProps) => (options: OptionalErrorProps) => _error(name, { ...options, ...overrides })
+export const ErrorAs = <E extends DefaultErrorTypes>(name: E, overrides?: OptionalErrorProps) =>
+  (options: OptionalErrorProps) => _error(name, { ...options, ...overrides })
 
 
