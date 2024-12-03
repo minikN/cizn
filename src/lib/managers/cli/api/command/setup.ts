6a5d9ad145bdf163c1b538b073b49635c2a6405b
Demis Balbach <db@minikn.xyz>
2024-12-03 23:08:10 +0100

    managers: cli: Converts setup helper to using pipes

--- src/lib/managers/cli/api/command/setup.ts
+++ src/lib/managers/cli/api/command/setup.ts
@@ -1,33 +1,75 @@
 import { Environment } from "@cizn/core/state"
+import {
+  bind, map, tap,
+} from "@lib/composition/function"
+import { pipe } from "@lib/composition/pipe"
+import { Failure, Success } from "@lib/composition/result"
+import { ErrorWith } from '@lib/errors'
 import { CliCommandProps } from "@lib/managers/cli/api"
 import { isStr } from "@lib/util"
 import path from "node:path"
 
 /**
- * Helper function to set the `environment` an command `options` provided
- * by the user.
+ * Sets the application `source` to the provided value if it's available.
  *
- * @param {Cizn.Application} app the main application
+ * This is a side effect and has no return value.
+ *
+ * @param {Cizn.Application} app the application
+ * @returns {undefined}
+ */
+const setSource = (app: Cizn.Application) => ({ options }: { options: CliCommandProps, environment: Environment }) => {
+  if (options?.source) {
+    app.State.Source.Current = options.source
+    app.State.Source.Root = path.dirname(options.source)
+  }
+}
+
+/**
+ * Checks if `environment` is a value not recognized by the application.
+ *
+ * Returns a {@link Failure} if so, otherwise returns the environment as a
+ * {@link Success}.
+ *
+ * @param {Object} props
+ * @param {Environment} props.environment the given environment
  * @returns
  */
-const _setup = (app: Cizn.Application) => async (environment: Environment, options: CliCommandProps) => {
-  const log = app.Manager.Log.Api
-  const { source } = options
+const ensureEnvironment = ({ environment, options }: { environment: Environment, options: CliCommandProps}) =>
+  isStr(environment) && environment !== 'home' && environment !== 'system'
+    ? Failure(ErrorWith('NOT_KNOWN_ENVIRONMENT', {
+      label: 'Environment given is not recognized. Can only be "home" or "system". Given value: %d',
+      options: [<string><unknown>environment],
+    }))
+    : Success({ environment, options })
 
-  if (source) {
-    app.State.Source.Current = source
-    app.State.Source.Root = path.dirname(source)
-  }
-
-  if (isStr(environment) && environment !== 'home' && environment !== 'system') {
-    log.error({
-      message: 'Environment given is not recognized. Can only be "home" or "system". Given value: %d',
-      options: [<any>environment],
-    })
-  }
-
+/**
+ * Sets the `environment`.
+ *
+ * @param {Cizn.Application} app the application
+ * @returns {Environment} the set environment
+ */
+const setEnvironment = (app: Cizn.Application) => (
+  { environment, options }: { environment: Environment, options: CliCommandProps},
+) => {
   app.State.Environment = environment
+
+  return { environment, options }
 }
+
+/**
+ * Sets up the `environment` given the provided user value.
+ *
+ * @param {Cizn.Application} app the application
+ * @returns {SuccessType<Environment>}
+ */
+const _setup = (app: Cizn.Application) => (
+  { environment, options }: { environment: Environment, options: CliCommandProps},
+) => pipe(
+  Success({ environment, options }),
+  tap(x => setSource(app)(x)),
+  map(ensureEnvironment),
+  bind(setEnvironment(app)),
+)
 
 export default _setup
 
