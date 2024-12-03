8755ed4f8bdc21ef12ef3e38832a9bcccc41550a
Demis Balbach <db@minikn.xyz>
2024-12-03 23:16:51 +0100

    managers: cli: Converts apply to using pipes

--- src/lib/managers/cli/api/apply.ts
+++ src/lib/managers/cli/api/apply.ts
@@ -1,54 +1,77 @@
-/* eslint-disable no-unused-vars */
 import { GenerationEnvironment } from '@cizn/core/generation/api'
 import { Environment } from '@cizn/core/state'
+import { guard, map } from '@lib/composition/function'
+import { asyncPipe } from '@lib/composition/pipe'
+import { Result, Success } from '@lib/composition/result'
+import { ErrorAs, isError } from '@lib/errors'
 import { def } from '@lib/util'
 import { CliCommandProps } from '.'
 import _setup from './command/setup'
 
 /**
- * Building the configuration
+ * If an environment is set, it will execute `callback` with the provided `options`.
+ * If not, it'll loop through all possible environments, set it per iteration and then
+ * execute `callback` with the provided `options`.
  *
- * @param {Cizn.Application} app the main application
- * @returns {Cizn.Adapter.Cli.Api['build']}
+ * @param {Cizn.Application} app  the application
+ * @param {Function} callback     the callback to apply for each environment
+ * @returns {undefined}
  */
-const apply = (app: Cizn.Application) => async (environment: Environment, options: CliCommandProps) => {
-  _setup(app)(environment, options)
+const withEnvironment = <E, V>(
+  app: Cizn.Application, callback: (a: Cizn.Application) => (options: CliCommandProps) => Result<E, V>) => async (
+    { environment, options }: {environment: Environment, options: CliCommandProps},
+  ) => {
+    if (!def(environment)) {
+      for (const currentEnv of ['home', 'system']) {
+        const env = <GenerationEnvironment>currentEnv
+        app.State.Environment = env
 
-  const log = app.Manager.Log.Api
-  const { Generation } = app.State
-
-  if (!def(environment)) {
-    for (const currentEnvironment of ['system', 'home']) {
-      app.State.Environment = <GenerationEnvironment>currentEnvironment
-
-      // app.State.Generation.Current[<GenerationEnvironment>currentEnvironment] = await Generation.Api.get(options.generation)
-      const generation = await Generation.Api.get(options.generation)
-
-      if (!generation) {
-        options.generation
-          ? log.error({ message: 'Could not find generation %d.', options: [options.generation] })
-          : log.error({ message: 'No generation to apply' })
-      } else {
-        await Generation.Api.set(generation)
-        await Generation.Api.apply()
+        const result = await callback(app)(options)
+        if (isError(result)) return result
       }
-
-    }
-  } else {
-    const generation = await Generation.Api.get(options.generation)
-
-    if (!generation) {
-      options.generation
-        ? log.error({ message: 'Could not find generation %d.', options: [options.generation] })
-        : log.error({ message: 'No generation to apply' })
+      app.State.Environment = null
     } else {
-      await Generation.Api.set(generation)
-      await Generation.Api.apply()
+      app.State.Environment = environment
+
+      const result = await callback(app)(options)
+      if (isError(result)) return result
     }
 
+    return Success(undefined)
   }
 
+/**
+ * Applies either the newest generation or the one denoted by the `--generation` args
+ * provided by the user.
+ *
+ * @param {Cizn.Application} app the application
+ * @returns {undefined}
+ */
+const applyGeneration = (app: Cizn.Application) => (options: CliCommandProps) => asyncPipe(
+  Success(options.generation),
+  map(app.State.Generation.Api.get),
+  map(app.State.Generation.Api.set),
+  map(guard(app.State.Generation.Api.apply, {
+    EACCES: ErrorAs('EACCES', {
+      reasons: [
+        'You didn\'t run the command with elevated privilegdes. Try prepending "sudo -E" to it',
+      ],
+    }),
+  })),
+)
 
-}
+/**
+ * Applies either the newest generation or the one denoted by the `--generation` args
+ * provided by the user.
+ *
+ * @param {Cizn.Application} app the application
+ * @returns {undefined}
+ */
+const apply = (app: Cizn.Application) => async (environment: Environment, options: CliCommandProps) => asyncPipe(
+  Success({ environment, options }),
+  map(_setup(app)),
+  map(withEnvironment(app, applyGeneration)),
+  result => app.Manager.Cli.Result = result,
+)
 
 export default apply
