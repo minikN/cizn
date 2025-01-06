f31389e7e0b3a470fd520e71e9167b16cb524a25
Demis Balbach <db@minikn.xyz>
2025-01-06 16:42:06 +0100

    cizn: derivation: Converts path method to using pipes

--- src/app/core/derivation/api/index.ts
+++ src/app/core/derivation/api/index.ts
@@ -4,27 +4,25 @@ import file from '@cizn/core/derivation/api/file/index.ts'
 import get from '@cizn/core/derivation/api/get.ts'
 import make from '@cizn/core/derivation/api/make.ts'
 import path from '@cizn/core/derivation/api/path.ts'
-import {
-  Derivation, DerivationData, FileDerivation,
-} from '@cizn/core/state.ts'
+import { Derivation, DerivationData, FileDerivation } from '@cizn/core/state.ts'
 import { Result } from '@lib/composition/result.ts'
 import { CiznError } from '@lib/errors/index.ts'
 
 export type DerivationPathProps = {
-  name: string,
+  name: string
   builder: Derivation['builder']
   hashParts: {
-    config?: Object,
-    args?: Object,
-    env: DerivationData,
-    module: Cizn.Application.State.Derivation.Module | string,
+    config?: Object
+    args?: Object
+    env: DerivationData
+    module: Cizn.Application.State.Derivation.Module | string
     inputs: Derivation[]
   }
 }
 
 export type DerivationPath = {
-  path: Cizn.Application.State.Derivation['path'],
-  exists: boolean,
+  path: Cizn.Application.State.Derivation['path']
+  exists: boolean
 }
 
 export type DerivationFileApi = {
@@ -41,50 +39,60 @@ export type Api = {
   make: (
     module: Cizn.Application.State.Derivation.FileModule,
     builder: Cizn.Application.State.Derivation['builder'],
-    env?: Cizn.Application.State.DerivationData
+    env?: Cizn.Application.State.DerivationData,
   ) => Cizn.Application.State.Derivation
 
-  get: (hash: Cizn.Application.State.Derivation['hash']) => Promise<Result<
-   | CiznError<"MALFORMED_DERIVATION_HASH">
-   | CiznError<"NOT_A_FILE">
-   | CiznError<"NO_CONTENT_GIVEN">
-   | CiznError<"INVALID_CONTENT_GIVEN">
-   | CiznError<"NO_PATH_GIVEN">
-   | CiznError<"INCORRECT_PATH_GIVEN">,
-   Derivation | undefined
-   >>
+  get: (hash: Cizn.Application.State.Derivation['hash']) => Promise<
+    Result<
+      | CiznError<'MALFORMED_DERIVATION_HASH'>
+      | CiznError<'NOT_A_FILE'>
+      | CiznError<'NO_CONTENT_GIVEN'>
+      | CiznError<'INVALID_CONTENT_GIVEN'>
+      | CiznError<'NO_PATH_GIVEN'>
+      | CiznError<'INCORRECT_PATH_GIVEN'>,
+      Derivation | undefined
+    >
+  >
 
-  path: (props: DerivationPathProps) => Promise<DerivationPath>
+  path: (props: DerivationPathProps) => Promise<
+    Result<
+      | CiznError<'INVALID_CONTENT_GIVEN'>
+      | CiznError<'NO_PATH_GIVEN'>
+      | CiznError<'INCORRECT_PATH_GIVEN'>,
+      DerivationPath
+    >
+  >
   clean: () => void
   file: DerivationFileApi
   builders: DerivationBuilderApi
 }
 
-const derivationApi = (app: Cizn.Application): Api => Object.create({}, {
-  make: {
-    value: make(app),
-    enumerable: true,
-  },
-  clean: {
-    value: clean(app),
-    enumerable: true,
-  },
-  path: {
-    value: path(app),
-    enumerable: true,
-  },
-  get: {
-    value: get(app),
-    enumerable: true,
-  },
-  builders: {
-    value: builders(app),
-    enumerable: true,
-  },
-  file: {
-    value: file(app),
-    enumerable: true,
-  },
-})
+const derivationApi = (app: Cizn.Application): Api =>
+  Object.create({}, {
+    make: {
+      value: make(app),
+      enumerable: true,
+    },
+    clean: {
+      value: clean(app),
+      enumerable: true,
+    },
+    path: {
+      value: path(app),
+      enumerable: true,
+    },
+    get: {
+      value: get(app),
+      enumerable: true,
+    },
+    builders: {
+      value: builders(app),
+      enumerable: true,
+    },
+    file: {
+      value: file(app),
+      enumerable: true,
+    },
+  })
 
 export default derivationApi
--- src/app/core/derivation/api/path.ts
+++ src/app/core/derivation/api/path.ts
@@ -1,34 +1,64 @@
 import G from '@cizn/constants.ts'
+import { DerivationPath, DerivationPathProps } from '@cizn/core/derivation/api/index.ts'
+import { bind, guard, map } from '@lib/composition/function.ts'
+import { asyncPipe } from '@lib/composition/pipe.ts'
+import { Failure, Success } from '@lib/composition/result.ts'
+import { Error, ErrorAs } from '@lib/errors/index.ts'
 import { makeHash, sanitizeMultilineString } from '@lib/util/string.ts'
-import { readdir } from 'node:fs/promises'
-import { DerivationPath } from '@cizn/core/derivation/api/index.ts'
 
 /**
- * Will return an object containing the `path` of a derivation as well as
- * a boolean denoting if the derivation already exists.
+ * Stringifies `hashParts`
+ *
+ * @param {Cizn.Application} app the application
+ * @returns {Result<CiznError<"INVALID_CONTENT_GIVEN"> | string>}
+ */
+const stringifyHashParts = (app: Cizn.Application) => (hashParts: DerivationPathProps['hashParts']) => {
+  const str = JSON.stringify({ ...hashParts, module: sanitizeMultilineString(hashParts.module.toString()) })
+
+  if (str) {
+    return Success(str)
+  }
+
+  return Failure(Error('INVALID_CONTENT_GIVEN'))
+}
+
+/**
+ * Tries to find the derivation matching `hash`.
+ *
+ * It'll return the found derivation (or `undefined`) alongside the incoming hash.
+ *
+ * @param {Cizn.Application} app the application
+ * @returns {{ derivation: string | null, hash: string }}
+ */
+const findExistingDerivation = (app: Cizn.Application) => (hash: string) =>
+  asyncPipe(
+    Success(app.State.Derivation.Root),
+    map(app.Manager.FS.Api.Directory.read),
+    map((derivations) =>
+      Success({
+        hash,
+        derivation: derivations.find((file) => file.includes(hash)),
+      })
+    ),
+  )
+
+/**
+ * Will return an object containing the `path` of a derivation as well as a boolean
+ * denoting if the derivation already exists.
  *
  * @param {Cizn.Application} app the application
  * @returns {DerivationPath}
  */
-const path = (app: Cizn.Application): Cizn.Application.State.Derivation.Api['path'] => async ({ hashParts, name }) => {
-  const { Derivation } = app.State
-  const {
-    module = () => {}, env = {}, args = {}, config = {}, inputs = [],
-  } = hashParts
-  const hashString = JSON.stringify({
-    args, env, config, inputs, module: sanitizeMultilineString(module.toString()),
-  })
-
-  const hash = makeHash(hashString)
-  const derivations = await readdir(Derivation.Root)
-
-  const existingDerivation = derivations.find(
-    file => file.includes(hash),
+const path = (app: Cizn.Application): Cizn.Application.State.Derivation.Api['path'] => async ({ hashParts, name }) =>
+  asyncPipe(
+    Success(hashParts),
+    map(guard(stringifyHashParts(app), { TypeError: ErrorAs('INVALID_CONTENT_GIVEN') })),
+    bind(makeHash),
+    map(findExistingDerivation(app)),
+    bind(({ derivation, hash }) => (<DerivationPath> {
+      path: derivation || `${hash}-${name}.${G.DRV_EXT}`,
+      exists: !!derivation,
+    })),
   )
-
-  const path = existingDerivation || `${hash}-${name}.${G.DRV_EXT}`
-
-  return <DerivationPath>{ path, exists: !!existingDerivation }
-}
 
 export default path
